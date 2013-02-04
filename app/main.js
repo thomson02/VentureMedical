2a5d334dd09887c13ebb8a4fb4dd4efb49ee42e2
Andrew Thomson <thomson02@hotmail.com>
2013-02-04 21:53:22 +0000

    fb auth

--- app/main.js
+++ app/main.js
@@ -9,7 +9,13 @@ requirejs(['jquery', 'backbone', 'router', 'facebook'],
                 status: true, // check the login status upon init?
                 cookie: true, // set sessions cookies to allow your server to access the session?
                 xfbml: true // parse XFBML tags on this page?
-            } );
+            });
+
+            FB.login(function(response) {
+                if (response.authResponse) {
+                    var accessToken = response.authResponse.accessToken;
+                }
+            });
 
             new Router({});
             Backbone.history.start({ pushState: false, root: "/index.html" });
