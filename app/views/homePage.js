d35e54b017dcb4bb74acb5930f310f627b5fc06d
Andrew Thomson <thomson02@hotmail.com>
2013-02-04 23:14:39 +0000

    updated bootstrap css. homepage now fetches fb posts.

--- app/views/homePage.js
+++ app/views/homePage.js
@@ -2,9 +2,10 @@ define([
     'underscore',
     'jquery',
     'backbone',
-    'text!templates/homePage.html'
+    'text!templates/homePage.html',
+    'text!templates/facebookPosts.html'
 ],
-    function(_, $, Backbone, Template) {
+    function(_, $, Backbone, Template, FacebookPostsTemplate) {
 
         var publics = {};
 
@@ -18,32 +19,22 @@ define([
                 $("#mainMenu li.home").addClass("active");
             },
 
-            applyFacePile: function(){
-
+            displayFacebookPosts: function(){
                 // get auth token
+                var that = this;
                 $.get('https://graph.facebook.com/oauth/access_token?client_id=152258138262472&client_secret=74f75f611a69cb0a2e47f1d2c897c5a2&grant_type=client_credentials', function(r){
-                    var access_token = console.log(r.split('=')[1]);
-                    $.getJSON('https://graph.facebook.com/VentureMedicalUk/posts?limit=5&access_token=' + access_token, function(response) {
-
+                    var access_token = r.split('=')[1];
+                    // get 5 posts
+                    $.getJSON('https://graph.facebook.com/VentureMedicalUk/posts?fields=created_time,description,message,name,picture,link&limit=10&access_token=' + access_token, function(response) {
+                        var posts = _.chain(response.data).filter(function(post){ return post.message; }).first(5).value();
+                        that.$("#facebookPosts").html(_.template(FacebookPostsTemplate, { posts: posts }));
                     });
                 });
-
-
-
-                /*
-                (function(d, s, id) {
-                    var js, fjs = d.getElementsByTagName(s)[0];
-                    if (d.getElementById(id)) return;
-                    js = d.createElement(s); js.id = id;
-                    js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=184906128237600";
-                    fjs.parentNode.insertBefore(js, fjs);
-                }(document, 'script', 'facebook-jssdk'));
-                */
             },
 
             render: function(){
                 this.$el.html(_.template(Template));
-                this.applyFacePile();
+                this.displayFacebookPosts();
             }
         });
 
